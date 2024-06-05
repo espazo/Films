eda51826f96c760df48becf67813056198a41e40
kevin <espazo@outlook.com>
2024-06-05 15:22:22 +0800

    feature: add movie entities

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,13 @@
-let sayhi = 'hey, kevin';
-console.log(sayhi);
+import {validate} from "class-validator";
+import {Movie} from "./entities/Movie";
 
-interface ISay {
-    sayhi(): void;
-}
+const m = new Movie();
+m.name = 'movie name';
+m.types = ['喜剧'];
+m.areas = ['中国大陆'];
+m.isClassic = true;
+m.timeLong = 1;
+
+validate(m).then((error) => {
+    console.log(error);
+});
