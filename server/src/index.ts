ebecd74b6d237c15a386a6e468a5da6fd82eb243
Gaius Julius Caesar <espazo@outlook.com>
2024-06-19 18:12:24 +0800

    feature: build project

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,14 @@ import 'reflect-metadata';
 import Express from 'express';
 import MovieRouter from './routes/MovieRoute';
 import UploadRouter from './routes/UploadRoute';
+import history from 'connect-history-api-fallback';
 
 const app = Express();
 
 app.use(Express.json());
 
+app.use(history());
+app.use('/', Express.static('public/build'));
 app.use('/upload', Express.static('public/upload'));
 app.use('/api/movie', MovieRouter);
 app.use('/api/upload', UploadRouter);
